0958c583d1f2a16ef65e224961209c8a1a04e245
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2024-01-08 01:43:38 +0400

    create reg page add route, css for it, add some features for login page and create loading comp

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { GoogleLogin } from "@react-oauth/google";
 import jwt_decode from "jwt-decode";
-import { useNavigate } from "react-router-dom";
-import BackgroundVideoComp from "./BackgroundVideo/BackgroundVideoComp";
+import { NavLink, useNavigate } from "react-router-dom";
+import BackgroundVideoComp from "../commonComponents/BackgroundVideo/BackgroundVideoComp";
+import ButtonComponent from "../commonComponents/ButtonComponent/ButtonComponent";
+import { SingleInputTimeRangeField } from "@mui/x-date-pickers-pro";
+import { TextField } from "@mui/material";
+
 export default function Login() {
   const navigate = useNavigate();
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isValidPassword, setisValidPassword] = useState(false);
+
   const responsGoogle = (response) => {
     let decodedHeader = jwt_decode(response.credential);
     const { name, sub, picture } = decodedHeader;
@@ -19,17 +26,63 @@ export default function Login() {
     localStorage.setItem("user", JSON.stringify(doc));
     navigate("/home");
   };
+  const onClickHandlerForEmail = () => {
+    setIsValidEmail(true);
+  };
+  const onClickHandlerForPassword = () => {
+    setisValidPassword(true);
+  };
   return (
     <div className="login-container">
       <div className="backVideoWithForm">
         <BackgroundVideoComp />
         <div className="form-container">
-          <GoogleLogin
-            text="signin_with"
-            shape="circle"
-            onSuccess={responsGoogle}
-            onError={responsGoogle}
-          />
+          <form className="login-form" onSubmit="">
+            <div className="input-container">
+              <TextField
+                autoComplete
+                fullWidth="true"
+                required="true"
+                color="info"
+                error={isValidEmail}
+                onChange={onClickHandlerForEmail}
+                id="standard-basic"
+                label="Email"
+                variant="standard"
+              />
+            </div>
+            <div className="input-container">
+              <TextField
+                autoComplete
+                fullWidth="true"
+                required="true"
+                color="info"
+                error={isValidPassword}
+                onChange={onClickHandlerForPassword}
+                id="standard-basic"
+                label="Password"
+                variant="standard"
+              />
+            </div>
+            <div className="button-container">
+              <ButtonComponent text="Sign in" type="submit" />
+              <GoogleLogin
+                text="signin_with"
+                type="icon"
+                shape="circle"
+                onSuccess={responsGoogle}
+                onError={responsGoogle}
+              />
+            </div>
+            <div className="line-with-text">
+              <hr className="line" />
+              <span className="or-text">Don't have a account ?</span>
+              <hr className="line" />
+            </div>
+            <div className="sign-up-container">
+              <NavLink to="/registration">Sign up</NavLink>
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -1,6 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Registration.css";
+import { GoogleLogin } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
+import { NavLink, useNavigate } from "react-router-dom";
+import BackgroundVideoComp from "../commonComponents/BackgroundVideo/BackgroundVideoComp";
+import ButtonComponent from "../commonComponents/ButtonComponent/ButtonComponent";
+import { TextField } from "@mui/material";
 
-export default function Registration() {
-  return <div>Registration</div>;
+export default function Login() {
+  const navigate = useNavigate();
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isValidPassword, setisValidPassword] = useState(false);
+
+  const responsGoogle = (response) => {
+    let decodedHeader = jwt_decode(response.credential);
+    const { name, sub, picture } = decodedHeader;
+    const doc = {
+      _id: sub,
+      _type: "user",
+      userName: name,
+      image: picture,
+    };
+
+    localStorage.setItem("user", JSON.stringify(doc));
+    navigate("/home");
+  };
+  const onClickHandlerForEmail = () => {
+    setIsValidEmail(true);
+  };
+  const onClickHandlerForPassword = () => {
+    setisValidPassword(true);
+  };
+  const uploadImage = (e) => {
+    const { type, name } = e.target.files[0];
+    console.log(e.target.files[0]);
+  };
+  return (
+    <div className="login-container">
+      <div className="backVideoWithForm">
+        <BackgroundVideoComp />
+        <div className="form-container">
+          <form className="reg-form" onSubmit="">
+            <div className="input-container">
+              <TextField
+                autoComplete
+                fullWidth="true"
+                required="true"
+                color="info"
+                error={isValidEmail}
+                onChange={onClickHandlerForEmail}
+                id="standard-basic"
+                label="Email"
+                variant="standard"
+              />
+            </div>
+            <div className="input-container">
+              <TextField
+                autoComplete
+                fullWidth="true"
+                required="true"
+                color="info"
+                error={isValidPassword}
+                onChange={onClickHandlerForPassword}
+                id="standard-basic"
+                label="Password"
+                variant="standard"
+              />
+            </div>
+            <div>
+              <input
+                type={"file"}
+                name="upload-image"
+                onChange={uploadImage}
+                className="w-0 h-0"
+              />
+            </div>
+            <div className="button-container">
+              <ButtonComponent text="Sign up" type="submit" />
+              <GoogleLogin
+                text="signin_with"
+                type="icon"
+                shape="circle"
+                onSuccess={responsGoogle}
+                onError={responsGoogle}
+              />
+            </div>
+            <div className="line-with-text">
+              <hr className="line" />
+              <span className="or-text">Already have a account</span>
+              <hr className="line" />
+            </div>
+            <div className="sign-up-container">
+              <NavLink to="/login">Sign in ?</NavLink>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
