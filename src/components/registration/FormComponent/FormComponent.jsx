4ff39308c0164a60c085f5e41e6e22a10047473a
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2024-01-09 23:53:27 +0400

    creating birthday component

--- src/components/registration/FormComponent/FormComponent.jsx
+++ src/components/registration/FormComponent/FormComponent.jsx
@@ -14,6 +14,7 @@ import {
   onClickHandlerForPasswordFunction,
   onSubmitHandlerForRegistration,
 } from "../../../handlers";
+import BirthdayDateComponent from "../BirthdayDateComponent/BirthdayDateComponent";
 
 export default function FormComponent({ image }) {
   const navigate = useNavigate();
@@ -69,6 +70,7 @@ export default function FormComponent({ image }) {
           label="Password"
           variant="standard"
         />
+        <BirthdayDateComponent />
       </div>
       <div className="button-container">
         <ButtonComponent text="Sign up" type="submit" />
