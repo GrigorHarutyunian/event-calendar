ee8bbf5ad60369f2822e330addaa0b7e57713149
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2024-02-14 02:59:07 +0400

    add motion(animation) to the calendarDeal component

--- src/components/preview/CalendarDeal/CalendarDeal.jsx
+++ src/components/preview/CalendarDeal/CalendarDeal.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import CalendarImage from "../CalendarImage/CalendarImage";
 import CalendarInfo from "../CalendarInfo/CalendarInfo";
 import "./CalendarDeal.css";
+import { motion } from "framer-motion";
 export default function CalendarDeal() {
   return (
-    <div className="calendar-deal-container">
+    <motion.div
+      whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+      transition={{ duration: 1.1 }}
+      className="calendar-deal-container"
+    >
       <CalendarImage />
       <CalendarInfo />
-    </div>
+    </motion.div>
   );
 }
--- src/components/preview/CalendarInfo/CalendarInfo.jsx
+++ src/components/preview/CalendarInfo/CalendarInfo.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 import "./CalendarInfo.css";
 export default function CalendarInfo() {
-  return <div className="calendar-info-container">CalendarInfo</div>;
+  return (
+    <div className="calendar-info-container">
+      <h2 className="calendar-info-paragraf">
+        You do the business,
+        <br />
+        we'll handle the schedule
+      </h2>
+      <div className="calendar-span-info-container">
+        <span className="calendar-span-info">
+          Introducing our new calendar website, designed to revolutionize your
+          scheduling experience. Effortlessly regulate your schedule, add
+          events, and receive email reminders: all in one place. Stay organized,
+          stay ahead.
+        </span>
+      </div>
+    </div>
+  );
 }
