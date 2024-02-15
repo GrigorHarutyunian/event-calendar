71b25072b842b27b25d167fc3ebc6205c664fa53
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2024-02-15 20:02:45 +0400

    change some css in header and homepage

--- src/components/commonComponents/Header/Header.jsx
+++ src/components/commonComponents/Header/Header.jsx
@@ -2,14 +2,7 @@ import React from "react";
 import "./Header.css";
 import { IsLoginComponent, LogoComponent } from "../index";
 import { BurgerMenu } from "../../home/burger/BurgerMenu";
-import { FormControl, Select, MenuItem } from "@mui/material";
-import Button from "@mui/material/Button";
-import { useDispatch } from "react-redux";
-import { GetEvents } from "../../../firebase/service/GetEvents";
-import { useSelector } from "react-redux";
-import { changeCalendarType } from "../../../redux/slices/calendarTypeSlice";
-import { selectedDay } from "../../../redux/slices/selectedDaySlice";
-import { thisMonth } from "../../../redux/slices/currentDateSlice";
+import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 export default function Header() {
   const dispatch = useDispatch();
@@ -25,38 +18,6 @@ export default function Header() {
         {isLoggedIn && myLocation.pathname === "/home" && <BurgerMenu />}
         <LogoComponent />
       </div>
-      {isLoggedIn && myLocation.pathname === "/home" && (
-        <>
-          <FormControl style={{ maxWidth: "sm", width: "150px" }}>
-            <Select
-              value={calendarForm}
-              onChange={(event) =>
-                dispatch(changeCalendarType(event.target.value))
-              }
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-            >
-              <MenuItem value={"Month"}>Month</MenuItem>
-              <MenuItem value={"Year"}>Year</MenuItem>
-              <MenuItem value={"Week"}>Week</MenuItem>
-            </Select>
-          </FormControl>
-          <Button
-            style={{
-              padding: "13px 23px",
-              color: "aqua",
-            }}
-            onClick={() => {
-              GetEvents(dispatch, day.toDateString(), userID);
-              dispatch(selectedDay(day.toDateString()));
-              dispatch(thisMonth());
-            }}
-            variant="outlined"
-          >
-            Today
-          </Button>
-        </>
-      )}
       <div className="main-header-time-info-container">
         {/* <TimeShowing /> */}
         <IsLoginComponent />
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -3,6 +3,12 @@ import "./HomePage.css";
 import "./calendar/eventsListDay/EventsDay.js";
 import { CSSTransition } from "react-transition-group";
 
+import { FormControl, Select, MenuItem } from "@mui/material";
+import Button from "@mui/material/Button";
+import { changeCalendarType } from "../../redux/slices/calendarTypeSlice";
+import { selectedDay } from "../../redux/slices/selectedDaySlice";
+import { thisMonth } from "../../redux/slices/currentDateSlice";
+
 import { EventsDay } from "./calendar/eventsListDay/EventsDay.js";
 
 import { Calendar } from "./calendar/Calendar.js";
@@ -27,6 +33,7 @@ import { InvitationsModal } from "./burger/invitationsModal/InvitationModal.js";
 import { Preloader } from "../commonComponents";
 
 export const HomePage = () => {
+  const day = new Date();
   const calendarForm = useSelector((store) => store.calendarType);
   const currentDateText = useSelector((store) => store.currentDate);
   const isLoggedIn = useSelector((store) => store.userIsLogin);
@@ -49,8 +56,48 @@ export const HomePage = () => {
   useIsLoggin(!isLoggedIn, "/login", isLoggedIn);
   const Inivations = useSelector((store) => store.invitation);
   return (
-    <>
+    <section className="home-page-container">
       <Preloader />
+      <div className="home-calendar-today-regulate-container">
+        <FormControl style={{ maxWidth: "sm", width: "150px" }}>
+          <Select
+            value={calendarForm}
+            onChange={(event) =>
+              dispatch(changeCalendarType(event.target.value))
+            }
+            style={{
+              background: "linear-gradient(to right, #2b3248, #7b88cc)",
+              borderRadius: "5px",
+              fontSize: "15px",
+              fontWeight: "600",
+            }}
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            variant="outlined"
+          >
+            <MenuItem value={"Month"}>Month</MenuItem>
+            <MenuItem value={"Year"}>Year</MenuItem>
+            <MenuItem value={"Week"}>Week</MenuItem>
+          </Select>
+        </FormControl>
+        <Button
+          style={{
+            padding: "13px 23px",
+            background: "linear-gradient(to right, #2b3248, #7b88cc)",
+            color: "aqua",
+            fontSize: "15px",
+            fontWeight: "600",
+          }}
+          onClick={() => {
+            GetEvents(dispatch, day.toDateString(), userID);
+            dispatch(selectedDay(day.toDateString()));
+            dispatch(thisMonth());
+          }}
+          variant="outlined"
+        >
+          Today
+        </Button>
+      </div>
       <div className="home-page">
         <CSSTransition
           in={thereIsModal}
@@ -99,6 +146,6 @@ export const HomePage = () => {
           <EventsDay userID={userID} />
         </div>
       </div>
-    </>
+    </section>
   );
 };
